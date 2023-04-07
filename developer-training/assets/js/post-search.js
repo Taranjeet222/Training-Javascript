b2e5e767268f3f3f716ff5f11bffc20c0a151681
taran <taran.surme@gmail.com>
2023-04-07 16:11:18 +0530

    javascript part completed.pagination and filtering added

--- developer-training/assets/js/post-search.js
+++ developer-training/assets/js/post-search.js
@@ -8,54 +8,61 @@ $.ajax({
   }
 });
 
-console.log(jsonData.posts);
-
 class posts{
     constructor(data){
-        this.title = [];
-        data.forEach(element => {
-            this.title.push(element.title);
-        });
-        this.desc = [];
-        data.forEach(element => {
-            this.desc.push(element.desc);
-        });
-        this.images = [];
-        data.forEach(element => {
-            this.images.push(element.img);
-        });
-        this.category = [];
-        data.forEach(element => {
-            this.category.push(element.category);
-        });
-        this.datetime = [];
-        data.forEach(element => {
-            this.datetime.push(element.datetime);
-        });
-        this.author = [];
-        data.forEach(element => {
-            this.author.push(element.author);
-        });
-        this.comment = [];
-        data.forEach(element => {
-            this.comment.push(element.comment_count);
-        });
+        if (data) {
+            this.title = [];
+            data.forEach(element => {
+                this.title.push(element.title);
+            });
+            this.desc = [];
+            data.forEach(element => {
+                this.desc.push(element.desc);
+            });
+            this.images = [];
+            data.forEach(element => {
+                this.images.push(element.img);
+            });
+            this.category = [];
+            data.forEach(element => {
+                this.category.push(element.category);
+            });
+            this.datetime = [];
+            data.forEach(element => {
+                this.datetime.push(element.datetime);
+            });
+            this.author = [];
+            data.forEach(element => {
+                this.author.push(element.author);
+            });
+            this.comment = [];
+            data.forEach(element => {
+                this.comment.push(element.comment_count);
+            });
+        } else {
+            this.title = [];
+            this.desc = [];
+            this.images = [];
+            this.category = [];
+            this.datetime = [];
+            this.author = [];
+            this.comment = [];
+        }
     }
 }
-
-const post = new posts(jsonData.posts);
-
+var post = new posts(jsonData.posts);
+console.log(post);
 class generateposts{
-    constructor(post){
-        this.currentpage = 2;
+    constructor(){
+        this.currentpage =2;
         this.post_page = 4;
         this.start = (this.currentpage-1)*this.post_page;
         this.end = this.start + this.post_page;
         this.total = post.images.length;
     }
-    displayPosts(post) {
+    displayPosts() {
         var container = document.getElementsByClassName("search-container")[0];
-        // var container = document.getElementsByClassName("search-wrapper")[0];
+        container.innerHTML = ""; 
         if (typeof post !== 'undefined' && post.images && post.title && post.desc && post.category && post.datetime && post.author && post.comment){
             for (var i = this.start; i < Math.min(this.end,this.total); i++) {
                 var card = document.createElement("div");
@@ -78,20 +85,28 @@ class generateposts{
                 `;
                 container.appendChild(card);
             }
-            // var nvbar = document.createElement("ul");
-            // nvbar.classList.add("navigation");
-            // let statement = '';
-            // for(let i = 0;i<Math.ceil(this.total/this.post_page);i++)
-            // {
-            //     statement += `
-            //         <li>i+1</li>
-            //     `
-            // }
+        }
+    }
+    displaynavigation() {
+        var nav_container = document.getElementsByClassName("navigation")[0];
+        nav_container.innerHTML = ``;
+        console.log(Math.ceil(this.total / this.post_page));
+        for (let i = 0; i < Math.ceil(this.total / this.post_page); i++) {
+          let no = i + 1;
+          var nvbar = document.createElement("button");
+          nvbar.setAttribute("data-no", no);
+          nvbar.addEventListener("click", () => {
+            this.currentpage = no;
+            this.start = (this.currentpage - 1) * this.post_page;
+            this.end = this.start + this.post_page;
+            this.displayPosts();
+          });
+          nvbar.innerHTML = `${no}`;
+          nav_container.appendChild(nvbar);
         }
     }
 }
-const get_post = new generateposts(post);
-get_post.displayPosts(post);
+
 class search {
     constructor(searchInputId, pst) {
       this.searchInput = document.getElementById(searchInputId);
@@ -104,13 +119,34 @@ class search {
     
     filterposts() {
       let searchText = this.searchInput.value.toLowerCase();
-      let filteredposts = this.pst.filter(function(post) {
-        let postTitle = post.title.toLowerCase();
-        return postTitle.includes(searchText);
-      });
-      get_post.displayPosts(filteredposts);
+      var filteredposts = new posts();
+      for(let i=0;i<this.pst.title.length;i++)
+      {
+        let match1 = this.pst.title[i].toLowerCase();
+        let match2 = this.pst.desc[i].toLowerCase();
+        if(match1.includes(searchText) || match2.includes(searchText))
+        {
+            filteredposts.title.push(this.pst.title[i]);
+            filteredposts.desc.push(this.pst.desc[i]);
+            filteredposts.images.push(this.pst.images[i]);
+            filteredposts.category.push(this.pst.category[i]);
+            filteredposts.datetime.push(this.pst.datetime[i]);
+            filteredposts.author.push(this.pst.author[i]);
+            filteredposts.comment.push(this.pst.comment[i]);
+        }
+      }
+      get_post.total = filteredposts.images.length;
+      get_post.currentpage = 1;
+      get_post.start = (get_post.currentpage-1)*get_post.post_page;
+      get_post.end = get_post.start + get_post.post_page;
+      post = filteredposts;
+      console.log(post);
+      get_post.displayPosts();
+      get_post.displaynavigation();
     }
 }
-const searchposts = new search();
-
+const searchposts = new search("srch",post);
+const get_post = new generateposts();
+get_post.displayPosts();
+get_post.displaynavigation();
 
